9dd29fb716917ae0d084a7941a9b0447b3f1d70a
lautaroh2394 <lautarohaase_23@hotmail.com>
2025-03-03 22:43:19 -0300

    add health doc

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('health')
 export class HealthController {
     @Get()
+    @ApiOperation({description: 'Simple endpoint to check if service is online'})
     healthCheck(){
         return 'ok!'
     }
