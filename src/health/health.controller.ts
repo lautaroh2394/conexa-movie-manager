28cb72affb56184fcfc6ca0f16aad9a7b0d9c2cd
lautaroh2394 <lautarohaase_23@hotmail.com>
2025-03-04 00:44:26 -0300

    set health endpoint to public

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
     @Get()
+    @Public()
     @ApiOperation({description: 'Simple endpoint to check if service is online'})
     healthCheck(){
         return 'ok!'
