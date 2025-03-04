cb1288a397cbe6fa5e8cce2da26e2ec131d6f727
lautaroh2394 <lautarohaase_23@hotmail.com>
2025-03-04 17:56:26 -0300

    add starwars unit tests

--- src/starwars/starwars.service.spec.ts
+++ src/starwars/starwars.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StarWarsService } from './starwars.service';
+import { expectedGetMovies, sw1, sw2 } from './test-constants';
 
 describe('StarwarsService', () => {
   let service: StarWarsService;
+  const mockedApiResponse = {
+    results: [
+      sw1,
+      sw2,
+    ]
+  }
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [StarWarsService],
-    }).compile();
-
-    service = module.get<StarWarsService>(StarWarsService);
+    service = new StarWarsService()
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('getMovies', () => {
+    it('should return movies from api', async ()=>{
+      jest.spyOn(global, 'fetch').mockResolvedValue({
+          json: () => Promise.resolve(mockedApiResponse)
+        } as Response
+      )
+  
+      const res = await service.getMovies()
+      expect(res).toEqual(expectedGetMovies)
+    })
+
+    it('should return empty list in case of any error', async ()=>{
+      jest.spyOn(global, 'fetch').mockResolvedValue({
+          json: () => Promise.reject(new Error())
+        } as Response
+      )
+
+      const res = await service.getMovies()
+      expect(res).toEqual([])
+    })
+  })
 });
--- src/starwars/starwars.service.ts
+++ src/starwars/starwars.service.ts
@@ -7,15 +7,21 @@ import { CreateMovieDto } from 'src/movies/dto/create-movie.dto';
 @Injectable()
 export class StarWarsService {
     async getMovies(): Promise<CreateMovieDto[]>{
-        const res = await (await fetch(SWAPI_ENDPOINT)).json() as unknown as StarWarsResponseDto;
-        const movies = res.results.map((movie: StarWarsMoviesDto) => ({
-                name: movie.title,
-                description: movie.opening_crawl,
-                director: movie.director,
-                producer: movie.producer,
-                releaseDate: movie.release_date
-        } as CreateMovieDto))
+        try {
+            const res = await (await fetch(SWAPI_ENDPOINT)).json() as unknown as StarWarsResponseDto;
+            const movies = res.results.map((movie: StarWarsMoviesDto) => ({
+                    name: movie.title,
+                    description: movie.opening_crawl,
+                    director: movie.director,
+                    producer: movie.producer,
+                    releaseDate: movie.release_date
+            } as CreateMovieDto))
 
-        return movies;
+            return movies;
+        }
+        catch (e){
+            // In case of error we will just not execute any update
+            return []
+        }
     }
 }
